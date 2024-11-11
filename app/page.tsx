df772af93141e660944c0333864189b64188f1b9
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-11-11 19:53:09 +0200

    init the project and add shadcn

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+          <Button size={'icon'}>
+            Click me
+          </Button>
+  );
+}
